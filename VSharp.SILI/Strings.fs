954d63a4dfa952a702ec556405d5b1cf4e6f7900
Misha Kosticyn <mishakosticyn@yandex.ru>
2017-12-01 12:39:08 +0300

    Added getHashCode function for strings

--- VSharp.SILI/Strings.fs
+++ VSharp.SILI/Strings.fs
@@ -1,6 +1,7 @@
 ﻿namespace VSharp
 
 open JetBrains.Decompiler.Ast
+open MemoryCell
 open Memory
 open Arrays
 
@@ -31,3 +32,51 @@ module internal Strings =
                 one :?> int = 1 && lower.[zero].value = zero && termType = Numeric typedefof<char> && typex = Numeric typedefof<char> ->
               Some(ConcreteStringArray(unbox length, contents))
         | _ -> None
+
+    let internal getHashCode metadata state term =
+
+        let contentIsConcrete xs =
+            Cps.Seq.foldlk (fun a (key, cell) k ->
+                match key.term, cell.value.term with
+                | Array({term = Concrete(one, _)}, {term = Concrete (length, _)}, _, [_, DefaultInstantiator _], contents, _, ArrayType (_, ConcreteDimension 1)), Concrete(y, _)
+                    when length :?> int = 1 && one :?> int = 1 ->
+                        match Terms.term contents.[MakeZeroAddress Metadata.empty].value with
+                        | Concrete(num, _) -> k <| Set.add (unbox num, unbox y) a
+                        | _ -> None
+                | _ -> None)
+                Set.empty
+                xs
+                (fun x -> Some x)
+
+        let fullArray length value xs : seq<int * char> =
+            let indices : Set<int> = Set.map fst xs |> Set.difference (Set.ofSeq [0 .. length - 1])
+            Seq.append <| Set.toSeq xs <| Seq.map (fun k -> k, value) indices
+
+        getKeyOfString metadata state term (fun state ->
+            Terms.term >> function
+            | Struct(fields, String) ->
+                match Terms.term fields.[MakeStringKey "System.String.m_FirstChar"].value with
+                //Almost concrete string
+                | ConcreteStringArray(length, contents) ->
+                    Heap.toSeq contents
+                    |> contentIsConcrete
+                    |> function
+                        //Concrete string
+                        | Some set ->
+                            let discardLast = Seq.rev >> Seq.tail >> Seq.rev in
+                            let time = tick() in
+                            fullArray length (unbox <| System.Activator.CreateInstance(typedefof<char>)) set
+                            |> Seq.sortBy (fun (k, v) -> k)
+                            |> discardLast
+                            |> Seq.map snd
+                            |> Seq.toArray
+                            |> System.String
+                            |> fun string -> MakeNumber (string.GetHashCode()) metadata, state
+                        //Partially-symbolic string
+                        //Can contain symbolic values
+                        | None -> refToInt term, state
+                //Full-symbolic string
+                //Can be made by "makeSymbolicInstance" function
+                | Array(_, _, _, _, _, _, _) -> refToInt term, state
+                | _ -> internalfail "wrong string!"
+            | _ -> internalfail "not a string!")
