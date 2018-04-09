3d1b79f0510d7b4f06bf3245a81d50e9944fc0bf
Misha Kosticyn <mishakosticyn@yandex.ru>
2018-04-09 23:55:26 +0300

    New library with classes for demo was added

---
+++ Demo/McCarthy91.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo
+{
+    public static class McCarthy91
+    {
+        public static int McCarthy(int n)
+        {
+            return n > 100 ? n - 10 : McCarthy(McCarthy(n + 11));
+        }
+
+        public static void CheckMc91(int x)
+        {
+            if (x <= 96 && McCarthy(x + 5) != 91)
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
