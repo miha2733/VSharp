a7b4045474a7d33fe2d126231faa7987e9656927
Misha Kosticyn <mishakosticyn@yandex.ru>
2018-05-28 17:41:04 +0300

    [test] string tests were added

--- VSharp.CSharpUtils/Tests/Strings.cs
+++ VSharp.CSharpUtils/Tests/Strings.cs
@@ -9,5 +9,29 @@ namespace VSharp.CSharpUtils.Tests
         {
             return String.Empty;
         }
+
+        public static string StringOfCharArray(char[] a)
+        {
+            return new string(a);
+        }
+
+        public static int EmptyString1(char[] a)
+        {
+            char[] t = null;
+            string s = new string(t);
+            return s.Length;
+        }
+
+        public static string HopHeyCharArray()
+        {
+            char[] a = {'a', 'b', 'c'};
+            return new string(a);
+        }
+
+        public static int NullLength()
+        {
+            string s = null;
+            return s.Length;
+        }
     }
 }
