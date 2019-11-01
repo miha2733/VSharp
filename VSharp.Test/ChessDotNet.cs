75e9da178c5d69674cb4543a7a70362ef6f8bddf
Yurii Kostyukov <kostyukov.yurii@gmail.com>
2019-11-01 17:04:13 +0300

    [test] ChessDotNET

---
+++ VSharp.Test/ChessDotNet.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Builders;
+using NUnit.Framework.Internal.Commands;
+using VSharp.Interpreter.IL;
+using ChessDotNet.Pieces;
+
+namespace VSharp.Test
+{
+    [TestFixture]
+    public class ChessDotNet
+    {
+        [Test]
+        public static void Test()
+        {
+            var name = "ChessDotNet";
+            var svm = new SVM(new ILInterpreter());
+            // SVM.ConfigureSimplifier(new Z3Simplifier()); can be used to enable Z3-based simplification (not recommended)
+//            svm.ConfigureSolver(new SmtSolverWrapper<Microsoft.Z3.AST>(new Z3Solver()));
+            var assembly = Assembly.Load(name);
+            var ignoredLibs = new List<string>();
+            svm.Run(assembly, ignoredLibs);
+        }
+
+    }
+}
